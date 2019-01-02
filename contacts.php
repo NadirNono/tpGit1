04a0c494fef8b721c59223462e865091337390d8
nadir <nadir.elamelsi@gmail.com>
2019-01-02 14:12:40 +0100

    Ajout de contacts.php

---
+++ contacts.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contacts</title>
+</head>
+<body>
+    <h1>Mes contacts</h1>
+    <ul>
+    <li>Freddo</li>
+    <li>Sarah</li>
+    <li>Robert</li>
+    </ul>
+    <a href="index.html">Accueil</a>
+</body>
+</html>
